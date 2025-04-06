7ca50977ed6471f1c211c70daf2b316dd2fdab42
Manisha varma <manishavkapex1@gmail.com>
2025-04-06 18:56:14 +0530

    best seller product add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Categories from '../components/Categories'
+import BestSeller from '../components/BestSeller'
 
 const Home = () => {
   return (
     <div className='mt-10'>
         <Hero />
         <Categories />
+        <BestSeller />
     </div>
   )
 }
