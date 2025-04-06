5753e208e2d8d0e8cad62dc67981b3d58081e18a
Manisha varma <manishavkapex1@gmail.com>
2025-04-06 14:40:58 +0530

    create navbar

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -7,7 +7,17 @@ export const AppContextProvider = ({ children }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
-  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  const [showUserLogin, setShowUserLogin] = useState(false);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
